3a8cb405d26bf4152193a31cd506153a9fb71663
Ivan Pisquiy <pisquiy22@gmail.com>
2023-03-12 22:45:08 -0600

    Practica final, agregando apartado de ordenado

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import xml.etree.ElementTree as ET
 
-def read_xml_file(file_path):
-    tree = ET.parse(file_path)
-    root = tree.getroot()
+# Función para obtener el código de un elemento Plataforma o Juego
+def get_code(element):
+    return element.find('codigo').text
 
-    # Hacer lo que sea necesario con el archivo XML aquí
-    # Por ejemplo, imprimir los elementos en la consola
-    for child in root:
-        print(child.tag, child.attrib)
-        for subchild in child:
-            print(subchild.tag, subchild.attrib, subchild.text)
+# Función para ordenar una lista de elementos
+def sort_elements(elements):
+    return sorted(elements, key=get_code)
+
+# Función para ordenar la lista de Plataformas y la lista de Juegos
+def sort_lists(xml):
+    lista_plataformas = xml.find('ListaPlataformas')
+    lista_juegos = xml.find('ListadoJuegos')
+
+    # Ordenar lista de Plataformas
+    lista_plataformas[:] = sort_elements(lista_plataformas)
+
+    # Ordenar lista de Juegos
+    for juego in lista_juegos:
+        plataformas = juego.find('Plataformas')
+        plataformas[:] = sort_elements(plataformas)
+
+    lista_juegos[:] = sort_elements(lista_juegos)
+
+# Función para leer el archivo XML y devolver el XML ordenado
+def sort_xml_file(file_path):
+    tree = ET.parse(file_path)
+    xml = tree.getroot()
+    sort_lists(xml)
+    tree.write("ejemplo_ordenado.xml", encoding="UTF-8", xml_declaration=True)
+    return ET.tostring(xml)
 
 file_path = input("Escriba el archivo que desea leer:")
 
-read_xml_file(file_path)
+sort_xml_file(file_path)
